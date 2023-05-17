dec856ee050b3c6dd219899e997a1b91bbdc55b2
suzy <suzy@users-MacBook-Pro-2.local>
2023-05-17 13:14:44 +0600

    practice11

---
+++ lesson11.dart
@@ -0,0 +1,17 @@
+// void main(){
+//   List<int> a = [1,2,3,4];
+//   List<String> names = ['Aluun','aluu'];
+
+//   a.forEach((element) {
+//     print(element * element);
+//   });
+//   var b = a.map((e) => e * e);
+//   print(a);
+//   print(b.toList());
+//   var c  = a.where((element) => element % 2 ==0);
+//   List x = names.where((element) => element.length == 5 && element[0] == 'A',
+//   ).toList();
+//   print(c);
+// }
+
+}
---
+++ lesson11practice2.dart
@@ -0,0 +1,45 @@
+
+void main(){
+  // for(var i = 1; i<5; i++){ //1
+  //   print(i);
+  // }
+
+
+  // for(int i = 5; i>=1;i--){ //2
+  //   print(i);
+  // }
+
+  // for(int i = 1; i<10;i++){ //3
+  //   int result = 3 * i;
+  //   print('3 * $i = $result');
+  // }
+
+
+  for(var i = 100; i<=200;i++){ //4
+    if(i % 17 == 0){
+      print(i);
+    }
+  }
+
+  // for(int i = 1; i<15; i++){ //5
+  //   int sum =  7 * i; 
+  //   print(sum);
+  // }
+
+  // int i = 1; //5
+  // while(i<14){
+  //   i++;
+  //   int result = 7*i;
+  //   print(result);
+  // }
+
+ 
+   int number = 1; //6
+
+   while(number<= 512){
+    print(number);
+    number *= 2;
+   }
+
+
+}
